65a705c77835b1bca029c29112714c813f3ab87d
LeonTranter <leon.tranter@gmail.com>
2017-07-25 16:06:22 +1000

    added tests and other stuff

--- strip.py
+++ strip.py
@@ -1,26 +1,37 @@
 import argparse
 
 def modifyLink(link, mainTag):
+	# ignore the utm and/or trailing slash
 	location = link.find("?utm")
 	if link[location-1] == "/":
 		location -=1
+	# find the actual url
 	link = link[:location]
+	# find the last slash in the url - the article title is the resource i.e. what comes after that
 	newireversed = link[::-1]
-	strloc = len(link) - newireversed.find("/")
-	title = link[strloc:].replace("-", " ")
+	titleLocation = len(link) - newireversed.find("/")
+	# strip hyphens and underscores
+	title = link[titleLocation:].replace("-", " ").replace("_", " ")
 	title = title[0].upper() + title[1:]
 	if title == "":
-		nextloc = len(link) - newireversed[strloc:].find("/")
+		nextloc = len(link) - newireversed[titleLocation:].find("/")
 		title = link[nextloc:].replace("-", " ")
+	# construct and return the whole thing
 	link = "%s: %s %s\n" % (title, link, mainTag)
 	return link
 
-parser = argparse.ArgumentParser(description="specify some options such as hashtag")
-parser.add_argument("--hashtag", help="pass in a hashtag", default="agile")
-args = parser.parse_args()
-mainTag = "#" + args.hashtag
+def getTag():
+	parser = argparse.ArgumentParser(description="specify some options such as hashtag")
+	parser.add_argument("--hashtag", help="pass in a hashtag", default="agile")
+	args = parser.parse_args()
+	return "#" + args.hashtag
 
-with open ("sourcelinks.txt", "r+") as f, ("targetlinks.txt", "w") as g:
-	links = f.readlines()
-	for i in links:
-		g.write(modifyLink(i, mainTag))
+def main():
+	mainTag = getTag()
+	with open ("sourcelinks.txt", "r+") as f, ("targetlinks.txt", "w") as g:
+		links = f.readlines()
+		for i in links:
+			g.write(modifyLink(i, mainTag))
+
+if __name__ == "__main__":
+	main()
--- striptest.py
+++ striptest.py
@@ -1,5 +1,32 @@
 import unittest
+import strip
+
 
 class TestLinks(unittest.TestCase):
-    def test_can_strip_a_link():
-        pass
+    def test_can_create_a_link(self):
+        testString = r"http://abc.com/someurl" + "\n"
+        expectedString = r"Someurl: http://abc.com/someurl #test" + "\n"
+        outputString  = strip.modifyLink(testString, "#test")
+        self.assertEqual(expectedString, outputString)
+
+    def test_can_strip_a_link(self):
+    	testString = r"http://abc.com/someurl?utm_source=Iterable" + "\n"
+    	expectedString = r"Someurl: http://abc.com/someurl #test" + "\n"
+    	outputString = strip.modifyLink(testString, "#test")
+    	self.assertEqual(expectedString, outputString)
+
+    def test_can_strip_hyphens(self):
+    	testString = r"http://abc.com/someurl/name-of-article" + "\n"
+    	expectedString = r"Name of article: http://abc.com/someurl/name-of-article #test" + "\n"
+    	outputString = strip.modifyLink(testString, "#test")
+    	self.assertEqual(expectedString, outputString)
+
+    def test_can_strip_underscores(self):
+    	testString = r"http://abc.com/someurl/name_of_article" + "\n"
+    	expectedString = r"Name of article: http://abc.com/someurl/name_of_article #test" + "\n"
+    	outputString = strip.modifyLink(testString, "#test")
+    	self.assertEqual(expectedString, outputString)
+
+
+if __name__ == "__main__":
+	unittest.main()
